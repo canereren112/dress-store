21dff0159463efb5ad9834d97b25a6f44939db4f
canereren112 <canereren112@gmail.com>
2016-08-31 22:11:20 +0200

    Base Entity class created

---
+++ src/main/java/com/eren/dstore/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.eren.dstore.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
